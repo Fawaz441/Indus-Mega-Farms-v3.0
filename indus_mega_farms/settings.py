5424fb1569fff6fabfb245a8e8f5759604a46b6f
Fawaz <abdulsalamfawaz4@gmail.com>
2020-11-06 03:51:20 +0100

    Email credentials

--- indus_mega_farms/settings.py
+++ indus_mega_farms/settings.py
@@ -161,8 +161,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'indusmegafarms@gmail.com'
-EMAIL_HOST_PASSWORD = 'zewmjrpkllyrzwws'
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
 django_heroku.settings(locals(),staticfiles=False)
 # del DATABASES['default']['OPTIONS']['sslmode']
