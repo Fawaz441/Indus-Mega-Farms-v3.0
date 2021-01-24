03898873e4eee91807d51d9ddc47599ce647bd50
Fawaz <abdulsalamfawaz4@gmail.com>
2021-01-24 15:24:13 +0100

    fixed asgi file

--- indus_mega_farms/asgi.py
+++ indus_mega_farms/asgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+from mode import dev
 
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indus_mega_farms.settings')
+if dev:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indus_mega_farms.settings.dev')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indus_mega_farms.settings.prod')
+    
 
 application = get_asgi_application()
