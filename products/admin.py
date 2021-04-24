08142ce4ba3d43ef5f17999fbaf2528da9ba95b4
Fawaz <abdulsalamfawaz4@gmail.com>
2021-04-24 18:07:11 +0100

    product admin

--- products/admin.py
+++ products/admin.py
@@ -7,11 +7,15 @@ class OrderAdmin(admin.ModelAdmin):
     def get_items(self, obj):
         return "\n".join([str(p.item.name) + str( p.quantity) for p in obj.order_items.all()])
 
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
 admin.site.register(ProductReview)
 admin.site.register(OrderedProduct)
 admin.site.register(Order,OrderAdmin)
 admin.site.register(CompanyOrder)
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(ProductImage)
 # chai
 
