431584f58c5c000fa3d2bc4ae79f47bfd5490095
Fawaz441 <abdulsalamfawaz4@gmail.com>
2022-09-02 12:54:36 +0100

    static files

--- indus_mega_farms/settings/prod.py
+++ indus_mega_farms/settings/prod.py
@@ -36,7 +36,9 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-STATIC_URL = 'https://%s/' % (AWS_S3_CUSTOM_DOMAIN)
+# STATIC_URL = 'https://%s/' % (AWS_S3_CUSTOM_DOMAIN)
+STATIC_URL = '/static/'
+
 
 # PAYSTACK
 
